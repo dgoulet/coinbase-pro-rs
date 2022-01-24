4c40372bf647b59b785b7ede6da9eedc326f2740
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-24 21:04:11 +0800

    Delete examples directory

--- examples/public_sync.rs
+++
@@ -1,7 +0,0 @@
-use coinbase_pro_rs::{Public, Sync, SANDBOX_URL};
-
-fn main() {
-    let client: Public<Sync> = Public::new(SANDBOX_URL);
-    let time = client.get_time().unwrap();
-    println!("Coinbase.time: {}", time.iso);
-}
